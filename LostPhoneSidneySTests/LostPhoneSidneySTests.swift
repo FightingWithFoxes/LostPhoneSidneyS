43c89d27de56d59506cfb9efc8484de1730307dd
Sid <Sid@bsmith-mbp.local>
2016-01-31 21:22:24 -0600

    Initial Commit

---
+++ LostPhoneSidneySTests/LostPhoneSidneySTests.swift
@@ -0,0 +1,36 @@
+//
+//  LostPhoneSidneySTests.swift
+//  LostPhoneSidneySTests
+//
+//  Created by Sid on 1/31/16.
+//  Copyright © 2016 Sid. All rights reserved.
+//
+
+import XCTest
+@testable import LostPhoneSidneyS
+
+class LostPhoneSidneySTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
